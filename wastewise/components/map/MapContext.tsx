f501e54f3c737a8276591dbead2c08ef900edd89
Ashleen Sidhu <Ssidhuash4@gmail.com>
2025-11-09 10:57:56 -0700

    update

--- wastewise/components/map/MapContext.tsx
+++ wastewise/components/map/MapContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 import mapboxgl from "mapbox-gl";
 
 export type MapContextShape = { map: mapboxgl.Map | null; ready: boolean };
